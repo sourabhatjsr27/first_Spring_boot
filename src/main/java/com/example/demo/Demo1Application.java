230eb44fd8067aa5c46d5a0b343e2e0dc3c4509d
sourabh27 <talk2sourabhthakre27@gmail.com>
2023-09-18 14:05:29 +0530

    First Commit

---
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class Demo1Application {
+	
+	@GetMapping
+	public String helloWorld() {
+		return "Andhera kayam rahe";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+		
+	}
+
+}
